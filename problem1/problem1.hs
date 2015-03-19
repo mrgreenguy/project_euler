ea80bed2144f4c5132cf797f962519e3889ae0cf
Gill <mrgreenguy@users.noreply.github.com>
2015-03-19 22:16:46 +0100

    solved first problem

--- problem1/problem1.hs
+++ problem1/problem1.hs
@@ -1,3 +1,11 @@
 -- divide 1000 by 5 & 3 use this numbers as upperbounds make array of 1- upperbound sum array
 
+divisors :: Int -> [Int]
+divisors x = [z * x | z <- [1..(div 999 x)]]
 
+
+x_no_y :: [Int] -> [Int] -> [Int]
+x_no_y x y = [z | z <- x, not (elem z y) ]
+
+result :: [Int] -> [Int] -> Int
+result x y = sum ((x_no_y x y)++y)
